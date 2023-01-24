2af8cc67c153ad68cf9f3e93381576129945efd6
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2023-01-24 10:53:09 +0300

    refactor(Analytic): Rename import

--- frontend/components/Analytics/Analytics.tsx
+++ frontend/components/Analytics/Analytics.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image';
-import anal from 'public/img/analytic.png';
+import analytic from 'public/img/analytic.png';
 
 type Props = {}
 
 export default function Analytics({}: Props) {
   return (
     <div className='pr-[570px] pb-[230px]'>
-        <Image src={anal} alt='' className='w-[600px] h-[500px] '/>
+        <Image src={analytic} alt='' className='w-[600px] h-[500px] '/>
     </div>
   )
 }
