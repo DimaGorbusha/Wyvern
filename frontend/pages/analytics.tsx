faab0cdca8cd074a9a971bdb7770c0f448e7ce00
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2023-01-24 10:50:12 +0300

    feat(Analytic): Add analytic page

---
+++ frontend/pages/analytics.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainPageLayout from 'layouts/MainPageLayout'
+import Analytics from 'components/Analytics/Analytics'
+
+
+type Props = {}
+
+export default function index({}: Props) {
+  return (
+    <div className='flex flex-col'>
+      <MainPageLayout title="Аналитика - Wyvern" address="164523, обл. Архангельская, г Северодвинск, пр-кт Ленина, д. 50, кв. 24" heading="Аналитика" subheading='Показания за месяц' children={<Analytics/>}/>
+    </div>
+  )
+}
