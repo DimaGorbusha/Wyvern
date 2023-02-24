91a4a0a5d1295da92073b2497ff7b4a13586cca1
DimaGorbusha <dmitrgor1@yandex.ru>
2023-02-24 15:10:13 +0500

    refactor(Logo): Move Logo components files

--- frontend/components/Logo/Logo.props.ts
+++
@@ -1,7 +0,0 @@
-import { DetailedHTMLProps, LinkHTMLAttributes } from 'react';
-
-interface Props extends DetailedHTMLProps<LinkHTMLAttributes<HTMLLinkElement>, HTMLLinkElement> {
-
-}
-
-export default Props;
