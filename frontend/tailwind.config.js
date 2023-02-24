959317c5a5693d7f0e211a8a5a1bd7b4ce6e3d67
DimaGorbusha <dmitrgor1@yandex.ru>
2023-02-24 22:26:30 +0500

    refactor(Styles): Add custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,20 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#0D4FDC',
+        'red': '#D9284D',
+        'orange': '#F87239',
+        'yellow': '#FFC55C',
+        'green': '#32B797',
+        'black': '#35302F',
+        'darkGray': '#494443',
+        'gray': '#8F8F8F',
+        'lightGray': '#B4B4B4',
+        'bg': '#F8F8F8',
+      }
+    },
   },
   plugins: [],
 }
