24d22d812249672f3d8c6b25b526a31037ae05f0
DimaGorbusha <dmitrgor1@yandex.ru>
2023-03-05 01:34:50 +0500

    refactor(Logo): Add @ in import

--- frontend/components/common/Logo/index.tsx
+++ frontend/components/common/Logo/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Props from './Logo.props';
-import LogoIcon from '../../../assets/common/ic_logo.svg';
+import LogoIcon from '@/assets/common/ic_logo.svg';
 
 const Logo: React.FC<Props> = ({ className = '' }) => {
     return (
